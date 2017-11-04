8600ba5d1c79de631b57286647433de09b2a66fd
OmniaGM <omnia.gamal1988@gmail.com>
2017-11-04 18:06:54 +0000

    Rename datasource

--- scripts/provision-monitoring.sh
+++ scripts/provision-monitoring.sh
@@ -7,4 +7,4 @@ echo "Generate Prometheus Config file ..."
 ./monitoring_config/prometheus/generater_config_yml.sh $(./scripts/get-ec2-ip/extract-ec2-ip.sh tag_Name_wordpress)
 
 echo "Provisioning Monitoring EC2 ..."
- ansible-playbook -i ./ansible/ec2.py --private-key=~/Downloads/demo.pem -v ./ansible/prom.yml
+ansible-playbook -i ./ansible/ec2.py --private-key=~/Downloads/demo.pem -v ./ansible/prom.yml
