bed31857d7275754ae6647713c3744f76a2080fe
OmniaGM <omnia.gamal1988@gmail.com>
2017-11-05 22:42:48 +0000

    Fix run-dev script

--- run-dev.sh
+++ run-dev.sh
@@ -5,4 +5,4 @@ IP=$(ipconfig getifaddr en0)
 ./monitoring_config/alertmanager/generater_config_yml.sh
 ./monitoring_config/prometheus/generater_config_yml.sh $IP
 
-docker-compose -f ./compose/docker-compose.yml -f ./compose/docker-compose.dev.yml -f ./compose/docker-compose-monitor.yml up -d
+docker-compose -f ./compose/docker-compose.yml -f ./compose/docker-compose-monitor.yml up -d
